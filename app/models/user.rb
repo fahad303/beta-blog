1fc3304f4da32c90517e05cc8315d71c3952857b
Fahad <fahad@theaworld.com>
2018-02-05 06:24:34 +0000

    user complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 4, maximum: 12}
-    validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:20, maximum:105}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:20, maximum:105}, format: {with: VALID_EMAIL_REGEX}
 end
